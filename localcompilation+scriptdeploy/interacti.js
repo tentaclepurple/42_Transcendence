ea419d950470cb6c8eb972f5764767dd676ce299
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-02-26 13:13:07 +0100

    m

---
+++ localcompilation+scriptdeploy/interacti.js
@@ -0,0 +1,31 @@
+const Web3 = require('web3');
+const fs = require('fs');
+
+// Configurar la conexión con Ganache (o la red Ethereum que estés usando)
+const web3 = new Web3('http://localhost:8545'); // Cambia la URL por la de tu nodo Ethereum
+
+// Leer el ABI del contrato
+const abi = JSON.parse(fs.readFileSync('output/SimpleStorage_sol_SimpleStorage.abi', 'utf8'));
+
+// Dirección del contrato desplegado
+const contractAddress = '0x...'; // Reemplaza con la dirección de tu contrato
+
+// Crear una instancia del contrato
+const contract = new web3.eth.Contract(abi, contractAddress);
+
+// Obtener una lista de cuentas de Ganache
+web3.eth.getAccounts()
+    .then(accounts => {
+        // Realizar una transacción para llamar a la función setData del contrato
+        return contract.methods.setData(42).send({ from: accounts[0] }); // Cambia el valor 42 por el que desees establecer
+    })
+    .then(() => {
+        // Llamar a la función getData del contrato para obtener el valor establecido
+        return contract.methods.getData().call();
+    })
+    .then((data) => {
+        console.log('El valor actual del contrato es:', data);
+    })
+    .catch((error) => {
+        console.error('Error al interactuar con el contrato:', error);
+    });
