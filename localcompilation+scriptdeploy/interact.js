dcebc20328629117ae800b3f0b4d6463a9bd1709
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-02-26 13:45:22 +0100

    m

--- localcompilation+scriptdeploy/interact.js
+++ localcompilation+scriptdeploy/interact.js
@@ -1,4 +1,4 @@
-const {Web3} = require('web3');
+const { Web3 } = require('web3');
 const fs = require('fs');
 
 // Configurar la conexión con Ganache (o la red Ethereum que estés usando)
@@ -13,31 +13,29 @@ const contractAddress = '0x4eEA49EA23Af169AB1B1743247D67338b9FCD772'; // Reempla
 // Crear una instancia del contrato
 const contract = new web3.eth.Contract(abi, contractAddress);
 
-// Obtener una lista de cuentas de Ganache
-web3.eth.getAccounts()
-    .then(accounts => {
+async function main() {
+    try {
+        // Obtener una lista de cuentas de Ganache
+        const accounts = await web3.eth.getAccounts();
+
         // Llamar a la función getData del contrato para obtener el valor actual
-        return contract.methods.getData().call({ from: accounts[0] });
-    })
-    .then((data) => {
+        const data = await contract.methods.getData().call({ from: accounts[0] });
         console.log('El valor actual del contrato es:', data);
         
         // Llamar a la función setData del contrato para establecer un nuevo valor
-        return contract.methods.setData(99).send({ 
+        await contract.methods.setData(99).send({ 
             from: accounts[0],
             gas: 2000000, // Limite de gas para la transacción
             gasPrice: '30000000000' // Precio del gas
         }); // Cambia el valor 99 por el que desees establecer
-    })
-    .then(() => {
         console.log('Se ha establecido un nuevo valor en el contrato.');
         
         // Llamar a la función getData del contrato nuevamente para obtener el valor cambiado
-        return contract.methods.getData().call({ from: accounts[0] }); // Debería añadirse 'accounts[0]' aquí también
-    })
-    .then((newData) => {
+        const newData = await contract.methods.getData().call({ from: accounts[0] });
         console.log('El valor actualizado del contrato es:', newData);
-    })
-    .catch((error) => {
+    } catch (error) {
         console.error('Error al interactuar con el contrato:', error);
-    });
+    }
+}
+
+main();
