7a052803578a081d4cdb149ccbaaa17733727c64
Iban Montero Martinez <imontero@c4r1s5.42urduliz.com>
2024-02-26 10:40:40 +0100

    m

--- localcompilation+scriptdeploy/SimpleStorage.sol
+++ localcompilation+scriptdeploy/SimpleStorage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.21;
+pragma solidity 0.8.19;
 
 contract SimpleStorage {
     uint256 private data;
--- localcompilation+scriptdeploy/compdep.js
+++ localcompilation+scriptdeploy/compdep.js
@@ -6,20 +6,33 @@ const solc = require('solc');
 // Leer el código fuente del contrato
 const contractCode = fs.readFileSync('SimpleStorage.sol', 'utf8');
 
-// Compilar el contrato
-const compiledContract = solc.compile(contractCode);
+// Compilar el contrato con solcjs
+const input = {
+    language: 'Solidity',
+    sources: {
+        'SimpleStorage.sol': {
+            content: contractCode
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': ['*']
+            }
+        }
+    }
+};
+
+const compiledContract = JSON.parse(solc.compile(JSON.stringify(input)));
 
 // Obtener el bytecode y el ABI del contrato compilado
-const bytecode = compiledContract.contracts[':SimpleStorage'].bytecode;
-const abi = JSON.parse(compiledContract.contracts[':SimpleStorage'].interface);
+const bytecode = compiledContract.contracts['SimpleStorage.sol']['SimpleStorage'].evm.bytecode.object;
+const abi = compiledContract.contracts['SimpleStorage.sol']['SimpleStorage'].abi;
 
 const provider = ganache.provider();
 
 // Conectar a la red de Ethereum (por ejemplo, utilizando Ganache)
-//const web3 = new Web3('http://localhost:8545');
 const web3 = new Web3(provider);
-
-//---
 
 // Obtener una lista de cuentas generadas por Ganache
 web3.eth.getAccounts()
@@ -50,23 +63,3 @@ web3.eth.getAccounts()
   .catch(error => {
     console.error('Error al obtener cuentas:', error);
   });
-
-/* // Obtener una instancia del objeto del contrato
-const Contract = new web3.eth.Contract(abi);
-
-// Desplegar el contrato
-Contract.deploy({
-    data: '0x' + bytecode,
-    arguments: [constructorArguments] // Si tu contrato tiene argumentos de constructor
-})
-.send({
-    from: '0xYourAddress', // La dirección desde la que deseas desplegar el contrato
-    gas: 2000000, // Limite de gas para el despliegue
-    gasPrice: '20000000000' // Precio del gas
-})
-.then((newContractInstance) => {
-    console.log('Contrato desplegado en la dirección:', newContractInstance.options.address);
-})
-.catch((error) => {
-    console.error('Error al desplegar el contrato:', error);
-}); */
